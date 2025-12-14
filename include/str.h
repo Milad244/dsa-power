18cd8cef7ea6dbcd2d37d0d7ac5f2aeaac6e2df3
“Milad244” <145618774+Milad244@users.noreply.github.com>
2025-12-13 23:41:03 -0800

    Finished str implementations

--- include/str.h
+++ include/str.h
@@ -2,6 +2,7 @@
 #define STR_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Returns the length of the string (0 if null)
 size_t str_len(const char* s);
@@ -15,5 +16,23 @@ char* str_cpy(char* dest, const char* src);
 // Returns the difference between the first difference of the two characters
 // (returns 0 if equal or both null, -1 if s1 is null and 1 if s2 is null)
 int str_cmp(const char* s1, const char* s2);
+
+// Turns all lowercase ASCII letters uppercase, in-place
+void str_to_upper(char* s);
+
+// Turns all uppercase ASCII letters lowercase, in-place
+void str_to_lower(char* s);
+
+// Returns a substring of the given string inclusive to start and end (returns NULL on invalid input)
+char* str_substring(const char* s, int start, int end);
+
+// Reverses the given string, in-place
+void str_reverse(char* s);
+
+// Checks if the given string is a palindrome
+bool str_is_palindrome(const char* s);
+
+// Returns the longest substring of the given string that is a palindrome
+char* str_longest_substring_palindrome(const char* s);
 
 #endif // STR_H
--- src/str.c
+++ src/str.c
@@ -1,5 +1,6 @@
 #include "../include/str.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 size_t str_len(const char* s) {
     if (s == NULL) return 0;
@@ -48,4 +49,107 @@ int str_cmp(const char* s1, const char* s2) {
     return (unsigned char)s1[i] - (unsigned char)s2[i];
 }
 
-// TODO: Substrings, reverse, palindrome, substring palindrome
+void str_to_upper(char* s) {
+    if (s == NULL) return;
+
+    int i = 0;
+    while (s[i] != '\0') {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = 'A' + s[i] - 'a';
+        }
+        i++;
+    }
+}
+
+void str_to_lower(char* s) {
+    if (s == NULL) return;
+
+    int i = 0;
+    while (s[i] != '\0') {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = 'a' + s[i] - 'A';
+        }
+        i++;
+    }
+}
+
+char* str_substring(const char* s, int start, int end) {
+    if (s == NULL || start > end || start < 0 || end >= (int) str_len(s)) return NULL;
+
+    int i = 0, j = start;
+    char* res = (char*) malloc(sizeof(char) * (end - start + 2)); // + 1 for inclusive and + 1 for null-term
+    if (res == NULL) return NULL;
+    while(j <= end) {
+        res[i++] = s[j++];
+    }
+    res[i] = '\0';
+    return res;
+}
+
+void str_reverse(char* s) {
+    if (s == NULL) return;
+
+    int left = 0, right = str_len(s) - 1;
+    char tmp;
+    while (left < right) {
+        tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+    }
+}
+
+bool str_is_palindrome(const char* s) {
+    if (s == NULL) return false;
+
+    int left = 0, right = str_len(s) - 1;
+    while (left < right) {
+        if (s[left++] != s[right--]) return false;
+    }
+
+    return true;
+}
+
+// Helper function for str_longest_substring_palindrome
+static int expand_around_center(const char* s, int left, int right, int n) {
+    while (left >= 0 && right < n && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1; // length = (right - left + 1) - 2. The -2 is for the overshoot of while loop
+}
+
+char* str_longest_substring_palindrome(const char* s) {
+    int n = str_len(s);
+    if (n == 0) {
+        char* res = (char*) malloc(sizeof(char));
+        if (res == NULL) return NULL; 
+        res[0] = '\0';
+        return res;
+    }
+
+    int start = 0, end = 0;
+    for (int i = 0; i < n; i++) {
+        // odd length palindrome
+        int len1 = expand_around_center(s, i, i, n);
+
+        // even length palindrome
+        int len2 = expand_around_center(s, i, i+1, n);
+
+        // the max len
+        int len = (len1 > len2) ? len1 : len2;
+        if (len > end - start + 1) { // new biggest substring palindrome
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+
+    char* res = (char*) malloc(sizeof(char) * (end - start + 2));
+    if (res == NULL) return NULL;
+
+    for (int i = 0; i < end - start + 1; i++) {
+        res[i] = s[start + i];
+    }
+    res[end - start + 1] = '\0';
+
+    return res;
+}

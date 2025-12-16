9f6866f1e832336f6461cb246e58996b4ca85c33
“Milad244” <145618774+Milad244@users.noreply.github.com>
2025-12-16 15:54:51 -0800

    Finished LL implementations

--- src/linked_list.c
+++ src/linked_list.c
@@ -10,20 +10,37 @@ LL_t* LL_create(void) {
     return list;
 }
 
+void LL_clear(LL_t* list) {
+    if (list == NULL) return;
+
+    node_t* node = list->head;
+    while (node != NULL) {
+        node_t* tmp = node;
+        node = node->next;
+        free(tmp);
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void LL_free(LL_t* list) {
+    LL_clear(list);
+    free(list);
+}
+
 void LL_add_to_head(LL_t* list, int item) {
     if (list == NULL) return;
 
     node_t* node = (node_t*) malloc(sizeof(node_t));
     if (node == NULL) return;
+
     node->data = item;
-    if (list->head == NULL) { // If no items
-        list->head = node;
+    node->next = list->head; // New head will point to old head
+    if (list->head == NULL) { // Adding into empty edge case
         list->tail = node;
-        node->next = NULL;
-    } else {
-        node->next = list->head; // New points to old head
-        list->head = node; // New becomes head
     }
+    list->head = node;
 }
 
 void LL_add_to_tail(LL_t* list, int item) {
@@ -31,23 +48,23 @@ void LL_add_to_tail(LL_t* list, int item) {
 
     node_t* node = (node_t*) malloc(sizeof(node_t));
     if (node == NULL) return;
+
     node->data = item;
     node->next = NULL;
-    if (list->tail == NULL) { // If no items
+    if (list->tail == NULL) { // Adding into empty edge case
         list->head = node;
-        list->tail = node;
     } else {
         list->tail->next = node; // Old tail points to new tail
-        list->tail = node; // New becomes tail
     }
+    list->tail = node;
 }
 
 int LL_remove_from_head(LL_t* list) {
     if (list == NULL || list->head == NULL) return -1;
     
     node_t* node = list->head;
-    list->head = list->head->next;
-    if (list->head == NULL) list->tail = NULL; // Handling edge case where list becomes empty
+    list->head = list->head->next; // Set new head to next element
+    if (list->head == NULL) list->tail = NULL; // List became empty, update tail
     int data = node->data;
     free(node);
     return data;
@@ -66,6 +83,7 @@ int LL_remove_from_tail(LL_t* list) {
         return data;
     }
 
+    // Otherwise go until curr is tail, delete curr, and make prev new tail
     node_t* prev = list->head;
     node_t* curr = prev->next;
     while (curr->next != NULL) {
@@ -92,10 +110,15 @@ size_t LL_get_size(LL_t* list) {
     return size;
 }
 
+bool LL_is_empty(LL_t* list) {
+    return list == NULL || list->head == NULL;
+}
+
 bool LL_are_equal(LL_t* list1, LL_t* list2) {
     if (list1 == list2) return true;
     if (list1 == NULL || list2 == NULL) return false;
 
+    // Go one step forward at a time and check if both are equal at each step
     node_t* curr1 = list1->head, *curr2 = list2->head;
     while (curr1 != NULL && curr2 != NULL) {
         if (curr1->data != curr2->data) return false;
@@ -104,7 +127,65 @@ bool LL_are_equal(LL_t* list1, LL_t* list2) {
     }
     
     if (curr1 != NULL || curr2 != NULL) return false; // one isn't empty
-    return true; // both empty so equal
+    return true; // both empty
+}
+
+int LL_index_of(LL_t* list, int item) {
+    if (list == NULL) return -1;
+
+    size_t index = 0;
+    node_t* curr = list->head;
+
+    while (curr != NULL) {
+        if (curr->data == item) return index;
+        curr = curr->next;
+        index++;
+    }
+
+    return -1;
+}
+
+node_t* LL_get_item(LL_t* list, size_t index) {
+    if (list == NULL) return NULL;
+
+    size_t count = 0;
+    node_t* curr = list->head;
+    while (curr != NULL && count < index) {
+        curr = curr->next;
+        count++;
+    }
+    return curr;
+}
+
+node_t* LL_get_middle(LL_t* list) {
+    if (list == NULL || list->head == NULL) return NULL;
+
+    size_t count = 0;
+    node_t* curr = list->head;
+    while (curr != NULL) {
+        curr = curr->next;
+        count++;
+    }
+
+    size_t mid = count / 2;
+    curr = list->head;
+    count = 0;
+    while (count < mid) {
+        curr = curr->next;
+        count++;
+    }
+
+    return curr;
+}
+
+void LL_map(LL_t* list, int (*map)(int)) {
+    if (list == NULL) return;
+
+    node_t* curr = list->head;
+    while (curr != NULL) {
+        curr->data = map(curr->data);
+        curr = curr->next;
+    }
 }
 
 void LL_reverse(LL_t* list) {
@@ -122,59 +203,4 @@ void LL_reverse(LL_t* list) {
         curr = next;
     }
     list->head = prev;
-}
-
-void LL_map(LL_t* list, int (*map)(int)) {
-    if (list == NULL) return;
-
-    node_t* curr = list->head;
-    while (curr != NULL) {
-        curr->data = map(curr->data);
-        curr = curr->next;
-    }
-}
-
-node_t* LL_get_item(LL_t* list, int index) {
-    if (list == NULL || index < 0) return NULL;
-
-    int count = 0;
-    node_t* curr = list->head;
-    while (curr != NULL && count < index) {
-        curr = curr->next;
-        count++;
-    }
-    return curr;
-}
-
-node_t* LL_get_middle(LL_t* list) {
-    if (list == NULL || list->head == NULL) return NULL;
-
-    int count = 0;
-    node_t* curr = list->head;
-    while (curr != NULL) {
-        curr = curr->next;
-        count++;
-    }
-
-    int mid = count / 2;
-    curr = list->head;
-    count = 0;
-    while (count < mid) {
-        curr = curr->next;
-        count++;
-    }
-
-    return curr;
-}
-
-void LL_free(LL_t* list) {
-    if (list == NULL) return;
-
-    node_t* node = list->head;
-    while (node != NULL) {
-        node_t* tmp = node;
-        node = node->next;
-        free(tmp);
-    }
-    free(list);
 }
